dcb9541bdfda0958fa549b8c2fe83fb37b2f9f93
VatsaRishu <VatsaRishu@users.noreply.github.com>
2021-01-12 18:33:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+//express app (instanciate)
+const app = express();
+
+
+
+//middleware & satic files directory
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// listen for requests
+app.listen(3000);
+
+// Register View Engine
+app.set('view engine', 'ejs');
+
+app.get('/home', (req, res) => {
+    // res.send('<h1>Hi from Rishav</h1>');
+  res.render('home');
+  });
+
+app.get('/', (req, res) => {
+  res.redirect('/Home');
+});
+
+app.post('/Login', (req, res) => {
+    console.log(req.body);
+    res.render("login",{name:req.body.name});
+  });
